17bac116c8ad7877714d4bf1c18b7325f2f69ddb
StevieApp <njorogesteve31@gmail.com>
2023-04-10 19:40:13 +0300

    final

---
+++ lib/blogger.ex
@@ -0,0 +1,22 @@
+defmodule Blogger do
+  @moduledoc """
+  Documentation for `Blogger`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Blogger.hello()
+      :world
+
+  """
+  def fetch_blog() do
+    HTTPoison.get!("https://medium.com/podiihq/quick-connect-to-your-amazon-ec2-linux-instance-through-the-command-line-6c682960ef91")
+  end
+
+  def hello(myarray) do
+    Enum.map(myarray, &(String.upcase(&1)))
+  end
+end
